95744c4a3b573807b6c80583e46451763469e3ec
Maximusvin <Maximusvin@gmail.com>
2020-12-13 12:08:05 +0200

    added create question

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,12 @@
 import { Component } from 'react';
+import is from 'is_js';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import s from './Auth.module.css';
 
 class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -22,12 +24,12 @@ class Auth extends Component {
         value: '',
         type: 'password',
         label: 'Пароль',
-        errorMessage: 'Введите корректный пароль',
+        errorMessage: 'Введите корректный пароль (мин. 6 символов)',
         valid: false,
         touched: false,
         validation: {
           required: true,
-          mimLength: 6,
+          minLength: 6,
         },
       },
     },
@@ -41,8 +43,43 @@ class Auth extends Component {
     event.preventDefault();
   };
 
+  validateControl = (value, validation) => {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  };
+
   onChangeHandler = (event, controlName) => {
-    console.log(`${controlName} ${event.target.value}`);
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formControls[controlName] = control;
+
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+    this.setState({ formControls, isFormValid });
   };
 
   renderInputs = () => {
@@ -72,14 +109,23 @@ class Auth extends Component {
 
           <form onSubmit={this.submitHandler} className={s.authForm}>
             {this.renderInputs()}
+            <div className={s.controls}>
+              <Button
+                type="success"
+                onClick={this.loginHandler}
+                disabled={!this.state.isFormValid}
+              >
+                Войти
+              </Button>
 
-            <Button type="success" onClick={this.loginHandler}>
-              Войти
-            </Button>
-
-            <Button type="primary" onClick={this.registerHandler}>
-              Зарегистрироваться
-            </Button>
+              <Button
+                type="primary"
+                onClick={this.registerHandler}
+                disabled={!this.state.isFormValid}
+              >
+                Зарегистрироваться
+              </Button>
+            </div>
           </form>
         </div>
       </div>
