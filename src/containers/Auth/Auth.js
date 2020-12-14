9d6e665854bd9a6a670b3dcb83b3b97eb7e6e09e
Maximusvin <Maximusvin@gmail.com>
2020-12-14 12:41:30 +0200

    added auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import is from 'is_js';
+import axios from 'axios';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import s from './Auth.module.css';
@@ -35,9 +36,41 @@ class Auth extends Component {
     },
   };
 
-  loginHandler = () => {};
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
 
-  registerHandler = () => {};
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD39l82wtm-DDdHgc93dmm6PooBlfmUK6Q',
+        authData,
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true,
+    };
+
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD39l82wtm-DDdHgc93dmm6PooBlfmUK6Q',
+        authData,
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   submitHandler = event => {
     event.preventDefault();
